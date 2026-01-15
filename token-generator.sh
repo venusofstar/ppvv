a7d23eb21282b90912e4d336288dcb62c41e5ccb
venusofstar <sumidomikeangelo24@gmail.com>
2026-01-15 10:06:01 +0800

    Refactor token generation and update output URL
    Updated token generation to use USER_IP and changed URL format.

--- token-generator.sh
+++ token-generator.sh
@@ -2,12 +2,14 @@
 
 SECRET="my_secret_key"
 STREAM="indianapacers.m3u8"
-CLIENT_IP="1.2.3.4"          # user IP
-EXPIRES=$(($(date +%s) + 21600)) # 6 hours
+USER_IP="1.2.3.4"
 
-TOKEN=$(echo -n "${EXPIRES}${STREAM}${CLIENT_IP} ${SECRET}" \
-  | openssl md5 -binary \
-  | openssl base64 \
-  | tr '+/' '-_' | tr -d '=')
+START=$(date +%s)
+END=$((START + 21600)) # 6 hours
 
-echo "http://YOUR-IP:8080/secure/${TOKEN}/${EXPIRES}/${STREAM}"
+TOKEN=$(echo -n "${END}${STREAM}${USER_IP} ${SECRET}" \
+ | openssl md5 -binary \
+ | openssl base64 \
+ | tr '+/' '-_' | tr -d '=')
+
+echo "https://strm.example.com/secure/${TOKEN}/${START}/${END}/${STREAM}"
