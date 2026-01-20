c7b71b81103ca073a6994648ad2f5c86e9b102c1
venusofstar <sumidomikeangelo24@gmail.com>
2026-01-20 11:37:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 // CHANNEL MAP (EDIT THIS)
 // ==============================
 const CHANNELS = {
-  "gsw.m3u8": "https://strm.poocloud.in/secure/wMzvzCwrqWLfBnOAKHMSXftBqRqcJSvQ/1768870800/1768895280/goldenstatewarriors/index.m3u8",
+  "gsw.m3u8": "https://strm.poocloud.in/secure/gJMGTgSpbUvJeFXUnRVWIIhcCeRvFtWW/1768870800/1768895280/goldenstatewarriors/bible-verses.best/tracks-v1a1/mono.ts.m3u8",
   "lakers.m3u8": "https://strm.example.com/live/lakers/index.m3u8",
   "nba1.m3u8": "https://strm.example.com/live/nba1/index.m3u8"
 };
